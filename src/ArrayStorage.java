8f7e8a22fda49512fd09dce79ead9bd202e535e1
Da-KI <84732254+Da-KI@users.noreply.github.com>
2021-08-24 19:56:31 +0500

    HW01

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,61 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            } else break;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume r :
+                storage) {
+            if (r == null) break;
+            if (r.uuid.equals(uuid)) return r;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        boolean isDeleted = false;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) break;
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[1 + i];
+                isDeleted = true;
+            }
+            if (isDeleted) {
+                storage[i] = storage[1 + i];
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result = new Resume[size()];
+        System.arraycopy(storage, 0, result, 0, result.length);
+        return result;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                count++;
+            } else break;
+        }
+        return count;
     }
 }
